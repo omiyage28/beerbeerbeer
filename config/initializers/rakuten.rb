01201ad9919f8fcbc14da7900606ed8fa1c70226
omiyage28 <nwsyt282828@gmail.com>
2021-11-03 11:43:11 +0900

    Rakuten API 修正　②

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |c|
-  c.application_id = 1004973311294156391
-  c.affiliate_id = 21ed3a28.96b93f52.21ed3a29.0e579fe2
+  c.application_id = '1004973311294156391'
+  c.affiliate_id = '21ed3a28.96b93f52.21ed3a29.0e579fe2'
 end
