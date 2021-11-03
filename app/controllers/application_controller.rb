1d4e77bae9ab20ce95948a94afe45aaaa5990a03
omiyage28 <nwsyt282828@gmail.com>
2021-11-03 15:11:55 +0900

    オリアプ⑦　ユーザー認証機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  
- 
+  before_action :basic_auth
+
 
   private
   def configure_permitted_parameters
@@ -12,6 +11,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
   end
 
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
 
 end
